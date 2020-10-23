3597d75f2296503f043dcd1e9d6048de5138bf68
eemmresen <eemmresen@gmail.com>
2020-10-23 14:23:38 +0300

    Add files via upload

---
+++ Mezun_Karti.Entities/person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mezun_Karti.Entities
+{
+    public class person
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public string departman { get; set; }
+
+        public int graduated_year { get; set; }
+
+        public string Master { get; set; }
+
+        public string job { get; set; }
+
+        public string job_position { get; set; }
+    }
+}
---
+++ Mezun_Karti.Entities/user.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mezun_Karti.Entities
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+    }
+}
